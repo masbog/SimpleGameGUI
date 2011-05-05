9a937b091cc5f17883277be7f45a2515c156bcde
masbog <masbogie@yahoo.com>
2011-05-05 17:58:26 +0700

    Masih prtotipe

---
+++ SimpleGameAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  SimpleGameAppDelegate.h
+//  SimpleGame
+//
+//  Created by masbog on 5/5/11.
+//  Copyright 2011 masbog inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SimpleGameAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+	NSTextField *p1Name;
+	NSTextField *p2Name;
+	
+	NSButton *p1Attack;
+	NSButton *p2Attack;
+	
+	NSTableView *p1Stat;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, retain) IBOutlet NSTextField *p1Name;
+@property (nonatomic, retain) IBOutlet NSTextField *p2Name;
+@property (nonatomic, retain) IBOutlet NSButton *p1Attack;
+@property (nonatomic, retain) IBOutlet NSButton *p2Attack;
+@property (nonatomic, retain) IBOutlet NSTableView *p1Stat;
+
+@end
---
+++ SimpleGameAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  SimpleGameAppDelegate.m
+//  SimpleGame
+//
+//  Created by masbog on 5/5/11.
+//  Copyright 2011 masbog inc. All rights reserved.
+//
+
+#import "SimpleGameAppDelegate.h"
+
+@implementation SimpleGameAppDelegate
+@synthesize window;
+
+@synthesize p1Name, p2Name;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	NSLog(@"saya sudah sampai");
+}
+
+@end
